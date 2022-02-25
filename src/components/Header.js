60754f76b843b71bc551b01e9e805e19de8e3a1b
Betelihem <Betelihem>
2022-02-24 20:24:12 -0800

    commit tree f17ecbf77c3105091cbcc21f7e5af6e4ef87d10d

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header({onDarkModeClick, isDarkMode}) {
+    // console.log(onDarkModeClick)
+  return (
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {/* {isDarkMode ? "Dark" : "Light"} Mode */}
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+    </header>
+  )
+}
+
+export default Header
